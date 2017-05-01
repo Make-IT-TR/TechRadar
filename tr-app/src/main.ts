d277feee43afe241c40f17607108b42d018b1eea
arnoud <damylen@gmail.com>
2017-05-01 22:29:48 +0200

    Integrated Google Analytics Plugin

--- tr-app/src/main.ts
+++ tr-app/src/main.ts
@@ -11,6 +11,7 @@ import 'd3';
 import { Aurelia } from 'aurelia-framework';
 import { PLATFORM } from 'aurelia-pal';
 import * as Bluebird from 'bluebird';
+import 'aurelia-google-analytics';
 
 // remove out if you don't want a Promise polyfill (remove also from webpack.config.js)
 Bluebird.config({ warnings: { wForgottenReturn: false } });
@@ -19,8 +20,23 @@ Bluebird.config({ warnings: { wForgottenReturn: false } });
 export async function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
-    .developmentLogging();
-
+    .developmentLogging()
+    .plugin(PLATFORM.moduleName('aurelia-google-analytics'), config => {
+			config.init('UA-98391880-1');
+			config.attach({
+				logging: {
+					enabled: true // Set to `true` to have some log messages appear in the browser console.
+				},
+				pageTracking: {
+					enabled: true // Set to `false` to disable in non-production environments.
+				},
+				clickTracking: {
+					enabled: true // Set to `false` to disable in non-production environments.
+				},
+				exceptionTracking: {
+					enabled: true // Set to `false` to disable in non-production environments.
+				}
+			})});
     
   // Uncomment the line below to enable animation.
   //aurelia.use.plugin(PLATFORM.moduleName('aurelia-animator-css'));
@@ -28,6 +44,8 @@ export async function configure(aurelia: Aurelia) {
 
   // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
   // aurelia.use.plugin(PLATFORM.moduleName('aurelia-html-import-template-loader'));
+
+ 
 
   await aurelia.start();
   await aurelia.setRoot(PLATFORM.moduleName('app'));
