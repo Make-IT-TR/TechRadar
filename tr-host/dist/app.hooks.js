90dde592a606e00ae00aa1220700d5684ad07bb9
damylen <damylen@gmail.com>
2018-02-13 12:11:49 +0100

    Updated .gitignore, include dist folder

---
+++ tr-host/dist/app.hooks.js
@@ -0,0 +1,32 @@
+// Application hooks that run for every service
+const logger = require('./hooks/logger');
+module.exports = {
+    before: {
+        all: [],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: []
+    },
+    after: {
+        all: [],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: []
+    },
+    error: {
+        all: [],
+        find: [],
+        get: [],
+        create: [],
+        update: [],
+        patch: [],
+        remove: []
+    }
+};
+//# sourceMappingURL=app.hooks.js.map
